7313244c9f2624cab2b7cfdd5f3251dac791a232
Dly27 <149893105+Dly27@users.noreply.github.com>
2025-03-13 19:06:07 +0000

    Added stepwise selection

--- main.R
+++ main.R
@@ -3,7 +3,7 @@ library(scales)
 
 data <- read.csv("~/oscar_winner_predictor/oscars.csv", header=TRUE)
 
-# Data prepration
+# Data preparation
 training_data <- data[-(1:10), ]
 training_data$Ch <- ifelse(training_data$Ch == 1, 1, 0)
 training_data$WR<- training_data$WR / 10
@@ -15,13 +15,24 @@ for (col in c("Nom", "Length", "Days")) {
 print(training_data)
 
 # Fit model
-model <- glm(Ch ~ . -Year -Name, data = training_data, family = binomial )
-summary(model)
+full_model <- glm(Ch ~ . -Year -Name -Pic -Anf -Gf2 -Animation -Docu -U, data = training_data, family = binomial )
+summary(full_model)
 
-# Print confidence intervals for different levels
-for (level in c(0.95, 0.975, 0.99)) {
-  cat("\nConfidence intervals at", level * 100, "% level:\n")
-  print(confint(model, level = level))
-}
+# Calculate confidence intervals
+confint(full_model)
+
+# Attempt stepwise selection to find an optimal model(based on AIC)
+backward_model <- step(full_model, direction = "backward", trace = 0)
+
+null_model <- glm(Ch ~ 1, data = training_data, family = binomial )
+forward_model <- step(null_model, scope = list(lower = null_model, upper = full_model), direction = "forward", trace = 0)
+
+stepwise_model <- stepwise_model <- step(full_model, direction = "both", trace = 0)
+
+summary(backward_model)
+summary(forward_model)
+summary(stepwise_model)
+
+
 
   
