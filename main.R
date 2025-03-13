a13d3710f3ebd45c58a1b80a51fb9ffb8c714a27
Dly27 <149893105+Dly27@users.noreply.github.com>
2025-03-13 13:59:49 +0000

    Update main.R

--- main.R
+++ main.R
@@ -7,10 +7,21 @@ data <- read.csv("~/oscar_winner_predictor/oscars.csv", header=TRUE)
 training_data <- data[-(1:10), ]
 training_data$Ch <- ifelse(training_data$Ch == 1, 1, 0)
 training_data$WR<- training_data$WR / 10
-training_data$Nom <- rescale(training_data$Nom)
-training_data$Length <- rescale(training_data$Length)
-training_data$Days <- rescale(training_data$Days)
-
+training_data$Ebert <- training_data$Ebert / 4
+for (col in c("Nom", "Length", "Days")) {
+  training_data[[col]] <- rescale(training_data[[col]])
+}
 
 print(training_data)
+
+# Fit model
 model <- glm(Ch ~ . -Year -Name, data = training_data, family = binomial )
+summary(model)
+
+# Print confidence intervals for different levels
+for (level in c(0.95, 0.975, 0.99)) {
+  cat("\nConfidence intervals at", level * 100, "% level:\n")
+  print(confint(model, level = level))
+}
+
+  
